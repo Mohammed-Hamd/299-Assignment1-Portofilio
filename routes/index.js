f86b1ea78ad067360006a11bcebbf0f0b4958999
Mohammed-Hamd <mohammed_hamd85@yahoo.com>
2023-06-11 16:38:25 -0400

    Changes to index.js

--- routes/index.js
+++ routes/index.js
@@ -13,35 +13,35 @@ app.set('view engine', 'ejs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/home');
+  res.render('Pages/home');
 });
 
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('pages/home');
+  res.render('Pages/home');
 });
 
 
 /* GET About page. */
 router.get('/about', function(req, res, next) {
-  res.render('pages/about');
+  res.render('Pages/about');
 });
 
 /* GET Products page. */
 router.get('/projects', function(req, res, next) {
-  res.render('pages/projects');
+  res.render('Pages/projects');
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('pages/services');
+  res.render('Pages/services');
 });
 
 
 /* GET Contact us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('pages/contact');
+  res.render('Pages/contact');
 });
 
 module.exports = router;
